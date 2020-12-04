98535222b5f73764ac1947173833fceab5574a21
Aleksei <amashuroff@gmail.com>
2020-12-04 14:46:16 +0300

    binary search

---
+++ SearchAlgs.md
@@ -0,0 +1,12 @@
+## Searching Algorithms
+
+---
+
+### Linear Search
+
+- from O(1) to O(n)
+
+### Binary search
+
+- Eliminate half of the remaining elements at a time
+- Only works on sorted arrays
--- index.js
+++ index.js
@@ -1,156 +1,92 @@
-// first simple recursive function
-function countDown(num) {
-  if (num <= 0) {
-    console.log("All done");
-    return;
-  }
-  console.log(num);
-  num--;
-  countDown(num);
-}
-
-// countDown(7);
-
-// second simple recursive function
-function sumRange(num) {
-  if (num === 1) return 1;
-
-  return num + sumRange(num - 1);
-}
-
-// console.log(sumRange(5));
-
-// factorial recursively
-
-function factorial(num) {
-  if (num === 1) return 1;
-
-  return num * factorial(num - 1);
-}
-
-// console.log(factorial(4));
-
-// HELPER METHOD RECURSION PATTERN
-
-function outer(input) {
-  let outerScopedVariable = [];
-
-  function helper(helperInput) {
-    // modify the outerScopedVariable
-    helper(helperInput--);
+// O(n)
+function linearSearch(nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) return i;
   }
 
-  helper(input);
-
-  return outerScopedVariable;
+  return -1;
 }
 
-// collect odd values
+// O(log n)
+function binarySearch(sortedArr, val) {
+  let left = 0;
+  let right = sortedArr.length - 1;
 
-function collectOddValues(numsArr) {
-  let result = []; // don't thrash global scope
+  while (left <= right) {
+    let middle = Math.floor((right + left) / 2);
+    if (sortedArr[middle] === val) {
+      return middle;
+    } else if (sortedArr[middle] < val) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
 
-  function helper(helperInput) {
-    if (helperInput.length === 0) {
-      return;
+  return -1;
+}
+
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 2));
+
+// Naive string search - O(n^2)
+function naiveStringSearch(str, target) {
+  // loop over the str
+  // remember current index
+  // match acts as a checker
+  for (let i = 0; i < str.length; i++) {
+    let match = 0;
+    let currentI = i;
+
+    // loop over the target str
+    // if match, increment match by 1
+    // increment str's index by one to check the next character
+    for (let j = 0; j < target.length; j++) {
+      if (str[currentI] !== target[j]) {
+        break;
+      }
+      match++;
+      currentI++;
     }
 
-    if (helperInput[0] % 2 !== 0) {
-      result.push(helperInput[0]);
+    if (match === target.length) return true;
+  }
+
+  return false;
+}
+
+// console.log(naiveStringSearch("wowzomg", "omg"));
+
+// Naive approach-2 O(n^2)
+function naiveStringSearch2(str, subStr) {
+  let count = 0;
+  //loop over the long string and stop if the remaining string after i is smaller than the small string
+  for (let i = 0; i < str.length && str.length - i >= subStr.length; i++) {
+    //check if the first character of the short string is the same as the current one in the long string
+    //then if true slice the long string to give a word with the same length as the small string
+    //then compare this string to the small string
+    //if all of this is true then increment
+    if (str[i] === subStr[0] && str.slice(i, i + subStr.length) === subStr)
+      count++;
+  }
+  return count;
+}
+
+// Naive approach instructor O(n^2)
+function naiveStringSearch3(str, subStr) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < target.length; j++) {
+      // add j to i to get the next char in str
+      if (target[j] !== str[i + j]) {
+        break;
+      }
+
+      if (j === target.length) {
+        count++;
+      }
     }
-
-    helper(helperInput.slice(1));
   }
 
-  helper(numsArr);
-
-  return result;
+  return count;
 }
-
-// console.log(collectOddValues([1, 2, 3, 4, 5, 6]));
-
-// PURE RECURSION
-
-function collectOddValuesPure(arr) {
-  let newArr = [];
-
-  if (arr.length === 0) {
-    return newArr;
-  }
-
-  if (arr[0] % 2 !== 0) {
-    newArr.push(arr[0]);
-  }
-
-  newArr = newArr.concat(collectOddValuesPure(arr.slice(1)));
-
-  return newArr;
-}
-
-// console.log(collectOddValuesPure([1, 2, 3, 4, 5, 6]));
-
-// problem-1
-// Write a function that accepts a base and exponent,
-// the function should return the power of the base to the exponent.
-// the function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents.
-function power(base, exp) {
-  if (exp === 0) return 1;
-
-  return base * power(base, exp - 1);
-}
-
-// console.log(power(2, 0)); // 1
-// console.log(power(2, 2)); // 4
-// console.log(power(2, 4)); // 16
-
-// problem-2
-// Write a function that accepts a number and returns a factorial of that number
-// 5! = 5 * 4 * 3 * 2 * 1
-function factorial(num) {
-  if (num === 1 || num === 0) return 1;
-
-  return num * factorial(num - 1);
-}
-
-// console.log(factorial(1)); // 1
-// console.log(factorial(2)); // 2
-// console.log(factorial(4)); // 24
-// console.log(factorial(7)); // 5040
-
-// problem-3
-// Write a function that takes in array and returns the product of them all
-function productOfArray(arr) {
-  if (arr.length === 1) return arr[0];
-
-  return arr[0] * productOfArray(arr.slice(1));
-}
-
-// console.log(productOfArray([1, 2, 3])); // 6
-// console.log(productOfArray([1, 2, 3, 10])); // 60
-
-// problem-4
-// Write a function that accepts a number and adds up all the numbers from 0
-// to the number passed to the function.
-function recursiveRange(num) {
-  if (num === 1) return 1;
-
-  return num + recursiveRange(num - 1);
-}
-
-// console.log(recursiveRange(6)); // 21
-// console.log(recursiveRange(10)); // 55
-
-// problem-5
-// Write a function that accepts a number and returns the n-th num
-// in the Fibonacci sequence --> 1,1,2,3,5,8... every number in sequence is the sum of the
-// previous 2 numbers
-function fib(num) {
-  if (num <= 2) return 1;
-
-  return fib(num - 1) + fib(num - 2);
-}
-
-console.log(fib(4)); // 3
-// fib(10); // 55
-// fib(28); // 317811
-// console.log(fib(35)); // 9227465
